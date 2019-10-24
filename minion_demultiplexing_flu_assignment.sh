de0e4bc9eab42ad33c7e61c14a2016bfd6fb6e4e
sociovirology <37191735+sociovirology@users.noreply.github.com>
2019-10-24 11:15:03 -0700

    Updating to download FASTQ from EBI

--- minion_demultiplexing_flu_assignment.sh
+++ minion_demultiplexing_flu_assignment.sh
@@ -2,84 +2,91 @@
 
 # New Demultiplexing with cutadapt
 
+#Download FASTQ sequencing file from SRA via EBI (Guppy HAC basecalling)
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR103/013/SRR10302213/SRR10302213_1.fastq.gz -O data/aiv_env_all.fastq.gz
+
+#Unzip 
+gunzip data/aiv_env_all.fastq.gz
+
 #First let's filter low quality reads
-cat aiv_env_all.fastq | NanoFilt -q 9 > aiv_env_all_filtered.fastq
+cat data/aiv_env_all.fastq | NanoFilt -q 9 > aiv_env_all_filtered.fastq
 
 #Then convert to FASTA
 sed -n '1~4s/^@/>/p;2~4p' aiv_env_all_filtered.fastq  > aiv_env_all_filtered.fasta
 
-cutadapt -g ATCAGTTATGGTGGTCCTACCAGCAAAAGCAGG --untrimmed-output untrimmed_aiv_env_pass_forward.fasta -e 0.3 -o aiv_env_pass_forward.fasta aiv_env_all.fasta > cutadapt_ONT_forward_report.txt
-#Reads with adapters: 83,654 (48.8%)
+cutadapt -g ATCAGTTATGGTGGTCCTACCAGCAAAAGCAGG --untrimmed-output untrimmed_aiv_env_pass_forward.fasta -e 0.3 -o aiv_env_pass_forward.fasta aiv_env_all_filtered.fasta > cutadapt_ONT_forward_report.txt
+#Total reads processed:                 143,716
+#Reads with adapters:                    89,571 (62.3%)
 
+#Save barcode demultiplexing info into report
 head -n 11 cutadapt_ONT_forward_report.txt >> demultiplexing_assignment_report.txt
 
 wc -l aiv_env_pass_forward.fasta
-#167308 forward_aiv_env_pass.fasta
+#179142 aiv_env_pass_forward.fasta
 
 wc -l untrimmed_aiv_env_pass_forward.fasta
-#175822 untrimmed_forward_aiv_env_pass.fasta
+#179142untrimmed_aiv_env_pass_forward.fasta
 
-#167308+175822 = 343130
-
+#179142+108290 = 287432
 #Matches total!
-wc -l aiv_env_pass.fasta 
-#343130 aiv_env_pass.fasta
+wc -l aiv_env_all_filtered.fasta 
+#287432 aiv_env_all_filtered.fasta
 
 cutadapt -a CCTGCTTTTGCTGGTAGGACCACCATAACTGAT --untrimmed-output untrimmed_reverse_aiv_env_pass.fasta -e 0.3 -o aiv_env_pass_reverse.fasta untrimmed_aiv_env_pass_forward.fasta > cutadapt_ONT_reverse_report.txt
-#This should be -a flag (YES! AUG 22)
-#Reads with adapters: 53,081 (60.4%)
+#Total reads processed:                  54,145
+#Reads with adapters:                    39,737 (73.4%)
 
+#Save barcode demultiplexing info into report
 head -n 11 cutadapt_ONT_reverse_report.txt >> demultiplexing_assignment_report.txt
 
 wc -l aiv_env_pass_reverse.fasta
-#106162 aiv_env_pass_reverse.fasta
-#Perhaps a little fewer than expected
+#79474 aiv_env_pass_reverse.fasta
 
 #Trim the Illumina Barcode from Forward reads
-cutadapt -a file:illumina_rev.fasta -e 0.0 -O 21 --untrimmed-output untrimmed_barcodes_forward_aiv_env_pass.fasta -o forward_{name}.fasta aiv_env_pass_forward.fasta > cutadapt_forward_barcode_report.txt
-#Reads with adapters: 11,689 (14.0%)
+cutadapt -a file:data/illumina_rev.fasta -e 0.05 -O 21 --untrimmed-output untrimmed_barcodes_forward_aiv_env_pass.fasta -o forward_{name}.fasta aiv_env_pass_forward.fasta > cutadapt_forward_barcode_report.txt
+#Total reads processed:                  89,571
+#Reads with adapters:                    38,331 (42.8%)
 
 head -n 11 cutadapt_forward_barcode_report.txt >> demultiplexing_assignment_report.txt
 
 wc -l untrimmed_barcodes_forward_aiv_env_pass.fasta 
-#143930 untrimmed_barcodes_forward_aiv_env_pass.fasta
-#Looks like a lot of reads missed!!
+#102480 untrimmed_barcodes_forward_aiv_env_pass.fasta
 
 #Trim the Illumina Barcode from Reverse reads
-cutadapt -g file:illumina_fwd.fasta -e 0.0 -O 8 --untrimmed-output untrimmed_barcodes_reverse_aiv_env_pass.fasta -o reverse_{name}.fasta aiv_env_pass_reverse.fasta > cutadapt_reverse_barcode_report.txt
-#Reads with adapters: 5,280 (9.9%)
+cutadapt -g file:data/illumina_fwd.fasta -e 0.0 -O 8 --untrimmed-output untrimmed_barcodes_reverse_aiv_env_pass.fasta -o reverse_{name}.fasta aiv_env_pass_reverse.fasta > cutadapt_reverse_barcode_report.txt
+#Total reads processed:                  39,737
+#Reads with adapters:                     9,631 (24.2%)
 
 head -n 11 cutadapt_reverse_barcode_report.txt >> demultiplexing_assignment_report.txt
 
 wc -l untrimmed_barcodes_reverse_aiv_env_pass.fasta
-#95602 untrimmed_barcodes_reverse_aiv_env_pass.fasta
-#Not too many...
+#60212 untrimmed_barcodes_reverse_aiv_env_pass.fasta
 
-#This was attempt to join forward and reverse reads into one file. REVISIT!! AUG 21
-#for file in forward_*.fasta; 
-#do
-#  cat $file > reverse_${file%.fasta}.fasta
-#done
-
-#for file in forward_*.fasta; 
-#do
-#  echo reverse_${file%.fasta}.fasta
-#done
-
-ls forward_*.fasta | awk -F'[.]' '{print $1}'| parallel -j+0 --eta 'blastn -db ../all_avian_flu -query {.}.fasta -out {.}.out  -max_target_seqs 1 -outfmt 6'
-
-wc -l forward_*.out >> demultiplexing_assignment_report.txt
-
-ls reverse_*.fasta | awk -F'[.]' '{print $1}'| parallel -j+0 --eta 'blastn -db ../all_avian_flu -query {.}.fasta -out {.}.out  -max_target_seqs 1 -outfmt 6'
-
-wc -l reverse_*.out >> demultiplexing_assignment_report.txt
-
-#Export as text and build a script!
-#Now export text files to serve as input for scripts
-
-#Export the reads assigned to each sample, remembering to divide by 4 because it's FASTQ not FASTA
+#Demultiplexing complete. Now export text files to serve as input for downstream scripts
+#Export the reads assigned to each sample, remembering to divide by 2 because it's FASTA
 wc -l reverse_*.fasta | awk '{print $1/2" " $2}' | sed '$d'  > demultiplexing_by_sample.txt
 wc -l forward_*.fasta | awk '{print $1/2" " $2}' | sed '$d' >> demultiplexing_by_sample.txt
+
+#Now looking up sequences against all avian influenza virus whole genome sequences from FluDB
+
+#Need to make a BLAST database using the FluDB
+#File is at data/all_avian_flu.fasta
+makeblastdb -in data/all_avian_flu.fasta -dbtype nucl -out all_avian_flu
+
+#This command uses GNU parallel to conduct BLAST searches in parallel for each demultiplexed file
+#First looking at forward read files
+ls forward_*.fasta | awk -F'[.]' '{print $1}'| parallel -j+0 --eta 'blastn -db all_avian_flu -query {.}.fasta -out {.}.out  -max_target_seqs 1 -outfmt 6'
+
+#Export info to our running report
+wc -l forward_*.out >> demultiplexing_assignment_report.txt
+
+##Second, let's do the same for reverse read files
+ls reverse_*.fasta | awk -F'[.]' '{print $1}'| parallel -j+0 --eta 'blastn -db all_avian_flu -query {.}.fasta -out {.}.out  -max_target_seqs 1 -outfmt 6'
+
+#Export info to our running report
+wc -l reverse_*.out >> demultiplexing_assignment_report.txt
+
+#Now export text files to serve as input for downstream scripts
 
 #Now add the number of reads matching the avian flu database to the file avian_blast_matches.txt
 wc -l reverse_*.out | awk '{print $1" " $2}' | sed '$d' > avian_blast_matches.txt
